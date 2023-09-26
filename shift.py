c0fa95cfa6278786e8b2b40c1a2e3afb802cb94b
Iyer <viiyer.ext@deloitte.com>
2023-09-26 10:49:10 +0530

    First commit

--- shift.py
+++ shift.py
@@ -1,7 +1,4 @@
-# Taking "gfg input file.txt" as input file
-# in reading mode
 with open("input.txt", "r") as input:
-
 	with open("output.txt", "w") as output:
 		for line in input:
 			output.write(line)
